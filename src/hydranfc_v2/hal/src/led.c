511c690e38036528f9f521e51d1b3f607f7fc9f9
bvernoux <bvernoux@gmail.com>
2020-09-12 12:27:41 +0200

    led.c cleanup code and fix warnings

--- src/hydranfc_v2/hal/src/led.c
+++ src/hydranfc_v2/hal/src/led.c
@@ -9,8 +9,8 @@
   *
   *        http://www.st.com/myliberty
   *
-  * Unless required by applicable law or agreed to in writing, software 
-  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied,
   * AND SPECIFICALLY DISCLAIMING THE IMPLIED WARRANTIES OF MERCHANTABILITY,
   * FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT.
@@ -28,14 +28,14 @@
  *
  *  \author
  *
- *  \brief Implementation for controlling shield LEDs functionality 
+ *  \brief Implementation for controlling shield LEDs functionality
  *
  */
- 
+
 /*!
- * 
+ *
  */
- 
+
 /*
 ******************************************************************************
 * INCLUDES
@@ -53,93 +53,138 @@ volatile uint16_t msLedAP2P = 0;
 void ledOn(st25R3911Led_t Led)
 {
 #ifdef _NUCLEO_
-  switch(Led){
-    case LED_A:     HAL_GPIO_WritePin(LED_A_GPIO_Port     , LED_A_Pin     , GPIO_PIN_SET); break;
-    case LED_B:     HAL_GPIO_WritePin(LED_B_GPIO_Port     , LED_B_Pin     , GPIO_PIN_SET); break;
-    case LED_F:     HAL_GPIO_WritePin(LED_F_GPIO_Port     , LED_F_Pin     , GPIO_PIN_SET); break;
-    case LED_V:     HAL_GPIO_WritePin(LED_V_GPIO_Port     , LED_V_Pin     , GPIO_PIN_SET); break;
-    case LED_AP2P:  HAL_GPIO_WritePin(LED_AP2P_GPIO_Port  , LED_AP2P_Pin  , GPIO_PIN_SET); break;
-    default:
-      break;
-  }
+	switch(Led) {
+	case LED_A:
+		HAL_GPIO_WritePin(LED_A_GPIO_Port, LED_A_Pin, GPIO_PIN_SET);
+		break;
+	case LED_B:
+		HAL_GPIO_WritePin(LED_B_GPIO_Port, LED_B_Pin, GPIO_PIN_SET);
+		break;
+	case LED_F:
+		HAL_GPIO_WritePin(LED_F_GPIO_Port, LED_F_Pin, GPIO_PIN_SET);
+		break;
+	case LED_V:
+		HAL_GPIO_WritePin(LED_V_GPIO_Port, LED_V_Pin, GPIO_PIN_SET);
+		break;
+	case LED_AP2P:
+		HAL_GPIO_WritePin(LED_AP2P_GPIO_Port, LED_AP2P_Pin, GPIO_PIN_SET);
+		break;
+	default:
+		break;
+	}
+#else
+	(void)Led;
 #endif
 }
 
 void ledOff(st25R3911Led_t Led)
 {
 #ifdef _NUCLEO_
-  switch(Led){
-    case LED_A:     HAL_GPIO_WritePin(LED_A_GPIO_Port     , LED_A_Pin     , GPIO_PIN_RESET); break;
-    case LED_B:     HAL_GPIO_WritePin(LED_B_GPIO_Port     , LED_B_Pin     , GPIO_PIN_RESET); break;
-    case LED_F:     HAL_GPIO_WritePin(LED_F_GPIO_Port     , LED_F_Pin     , GPIO_PIN_RESET); break;
-    case LED_V:     HAL_GPIO_WritePin(LED_V_GPIO_Port     , LED_V_Pin     , GPIO_PIN_RESET); break;
-    case LED_AP2P:  HAL_GPIO_WritePin(LED_AP2P_GPIO_Port  , LED_AP2P_Pin  , GPIO_PIN_RESET); break;
-    default:
-      break;
-  }
+	switch(Led) {
+	case LED_A:
+		HAL_GPIO_WritePin(LED_A_GPIO_Port, LED_A_Pin, GPIO_PIN_RESET);
+		break;
+	case LED_B:
+		HAL_GPIO_WritePin(LED_B_GPIO_Port, LED_B_Pin, GPIO_PIN_RESET);
+		break;
+	case LED_F:
+		HAL_GPIO_WritePin(LED_F_GPIO_Port, LED_F_Pin, GPIO_PIN_RESET);
+		break;
+	case LED_V:
+		HAL_GPIO_WritePin(LED_V_GPIO_Port, LED_V_Pin, GPIO_PIN_RESET);
+		break;
+	case LED_AP2P:
+		HAL_GPIO_WritePin(LED_AP2P_GPIO_Port, LED_AP2P_Pin, GPIO_PIN_RESET);
+		break;
+	default:
+		break;
+	}
+#else
+	(void)Led;
 #endif
 }
 
 void ledToggle(st25R3911Led_t Led)
 {
 #ifdef _NUCLEO_
-  switch(Led){
-    case LED_A:     HAL_GPIO_TogglePin(LED_A_GPIO_Port     , LED_A_Pin     ); break;
-    case LED_B:     HAL_GPIO_TogglePin(LED_B_GPIO_Port     , LED_B_Pin     ); break;
-    case LED_F:     HAL_GPIO_TogglePin(LED_F_GPIO_Port     , LED_F_Pin     ); break;
-    case LED_V:     HAL_GPIO_TogglePin(LED_V_GPIO_Port     , LED_V_Pin     ); break;
-    case LED_AP2P:  HAL_GPIO_TogglePin(LED_AP2P_GPIO_Port  , LED_AP2P_Pin  ); break;
-    default:
-      break;
-  }
+	switch(Led) {
+	case LED_A:
+		HAL_GPIO_TogglePin(LED_A_GPIO_Port, LED_A_Pin     );
+		break;
+	case LED_B:
+		HAL_GPIO_TogglePin(LED_B_GPIO_Port, LED_B_Pin     );
+		break;
+	case LED_F:
+		HAL_GPIO_TogglePin(LED_F_GPIO_Port, LED_F_Pin     );
+		break;
+	case LED_V:
+		HAL_GPIO_TogglePin(LED_V_GPIO_Port, LED_V_Pin     );
+		break;
+	case LED_AP2P:
+		HAL_GPIO_TogglePin(LED_AP2P_GPIO_Port, LED_AP2P_Pin  );
+		break;
+	default:
+		break;
+	}
+#else
+	(void)Led;
 #endif
 }
 
-
-
 void ledOnOff(st25R3911Led_t Led, uint32_t delay)
 {
-  ledOn(Led);
+	ledOn(Led);
 
-  switch(Led){
-    case LED_A:     msLedA = delay; break;
-    case LED_B:     msLedB = delay; break;
-    case LED_F:     msLedF = delay; break;
-    case LED_V:     msLedV = delay; break;
-    case LED_AP2P:  msLedAP2P = delay; break;
-    default:
-      break;
-  }  
+	switch(Led) {
+	case LED_A:
+		msLedA = delay;
+		break;
+	case LED_B:
+		msLedB = delay;
+		break;
+	case LED_F:
+		msLedF = delay;
+		break;
+	case LED_V:
+		msLedV = delay;
+		break;
+	case LED_AP2P:
+		msLedAP2P = delay;
+		break;
+	default:
+		break;
+	}
 }
+
 void ledFeedbackHandler()
 {
-  if(msLedA > 0){
-    msLedA--;
-    if(msLedA == 0)
-      ledOff(LED_A);
-  }
-  
-  if(msLedB > 0){
-    msLedB--;
-    if(msLedB == 0)
-      ledOff(LED_B);
-  }   
-  
-  if(msLedF > 0){
-    msLedF--;
-    if(msLedF == 0)
-      ledOff(LED_F);
-  }  
-  
-  if(msLedV > 0){
-    msLedV--;
-    if(msLedV == 0)
-      ledOff(LED_V);
-  }    
-  
-  if(msLedAP2P > 0){
-    msLedAP2P--;
-    if(msLedAP2P == 0)
-      ledOff(LED_AP2P);
-  }  
+	if(msLedA > 0) {
+		msLedA--;
+		if(msLedA == 0)
+			ledOff(LED_A);
+	}
+
+	if(msLedB > 0) {
+		msLedB--;
+		if(msLedB == 0)
+			ledOff(LED_B);
+	}
+
+	if(msLedF > 0) {
+		msLedF--;
+		if(msLedF == 0)
+			ledOff(LED_F);
+	}
+
+	if(msLedV > 0) {
+		msLedV--;
+		if(msLedV == 0)
+			ledOff(LED_V);
+	}
+
+	if(msLedAP2P > 0) {
+		msLedAP2P--;
+		if(msLedAP2P == 0)
+			ledOff(LED_AP2P);
+	}
 }
